8dbac8a791e2cf8be8c5ce697778cfc205a21228
Jiayuan MAO <maojiayuan@gmail.com>
2017-03-23 16:52:30 -0700

    [dev-mjy] draw experiments

---
+++ examples/generative-model/desc_draw_mnist.py
@@ -0,0 +1,174 @@
+# -*- coding:utf8 -*-
+# File   : desc_draw_mnist.py
+# Author : Jiayuan Mao
+# Email  : maojiayuan@gmail.com
+# Date   : 3/22/17
+# 
+# This file is part of TensorArtist
+
+from tartist.core import get_env, get_logger
+from tartist.core.utils.naming import get_dump_directory, get_data_directory
+from tartist.nn import opr as O, optimizer, summary
+
+import tensorflow as tf
+import draw_opr
+
+logger = get_logger(__file__)
+
+__envs__ = {
+    'dir': {
+        'root': get_dump_directory(__file__),
+        'data': get_data_directory('WellKnown/mnist')
+    },
+    'trainer': {
+        'learning_rate': 0.001,
+
+        'batch_size': 100,
+        'epoch_size': 50,
+        'nr_epochs': 100,
+    },
+    'inference': {
+        'batch_size': 256,
+        'epoch_size': 40
+    },
+    'demo': {
+        'is_reconstruct': True
+    }
+}
+
+
+def make_network(env):
+    with env.create_network() as net:
+        h, w, c = 28, 28, 1
+        nr_glimpse = 5
+        att_dim = 14
+        code_length = 128
+
+        is_reconstruct = get_env('demo.is_reconstruct', False)
+
+        dpc = env.create_dpcontroller()
+        with dpc.activate():
+            def inputs():
+                img = O.placeholder('img', shape=(None, h, w, c))
+                return [img]
+
+            def forward(img):
+                encoder = tf.contrib.rnn.BasicLSTMCell(256)
+                decoder = tf.contrib.rnn.BasicLSTMCell(256)
+                canvas = O.zeros_like(img)
+
+                batch_size = img.shape[0]
+                enc_state = encoder.zero_state(batch_size, dtype='float32')
+                dec_state = decoder.zero_state(batch_size, dtype='float32')
+                enc_h, dec_h = enc_state[1], dec_state[1]
+
+                def encode(x, state, reuse):
+                    with tf.variable_scope('read_encoder', reuse=reuse):
+                        return encoder(O.as_tftensor(x), state)
+
+                def decode(x, state, reuse):
+                    with tf.variable_scope('write_decoder', reuse=reuse):
+                        return decoder(O.as_tftensor(x), state)
+
+                all_sqr_mus, all_vars, all_log_vars = 0., 0., 0.
+
+                if is_reconstruct or env.phase is env.Phase.TRAIN:
+                    for step in range(nr_glimpse):
+                        reuse = (step != 0)
+                        img_hat = draw_opr.image_diff(img, canvas)
+
+                        with tf.variable_scope('read', reuse=reuse):
+                            read_param = O.fc('fc_param', enc_h, 5)
+
+                        with tf.name_scope('read_step{}'.format(step)):
+                            cx, cy, delta, var, gamma = draw_opr.split_att_params(h, w, att_dim, read_param)
+                            read_inp = O.concat([img, img_hat], axis=3)  # of shape: batch_size x h x w x (2c)
+                            read_out = draw_opr.att_read(att_dim, read_inp, cx, cy, delta, var)
+                            enc_inp = O.concat([gamma * read_out.flatten2(), dec_h], axis=1)
+                        enc_h, enc_state = encode(enc_inp, enc_state, reuse)
+
+                        with tf.variable_scope('sample', reuse=reuse):
+                            _ = enc_h
+                            sample_mu = O.fc('fc_mu', _, code_length)
+                            sample_log_var = O.fc('fc_sigma', _, code_length)
+
+                        with tf.name_scope('sample_step{}'.format(step)):
+                            sample_var = O.exp(sample_log_var)
+                            sample_std = O.sqrt(sample_var)
+                            sample_epsilon = O.random_normal([batch_size, code_length])
+                            z = sample_mu + sample_std * sample_epsilon
+
+                        z = O.callback_injector(z)
+
+                        # accumulate for losses
+                        all_sqr_mus += sample_mu ** 2.
+                        all_vars += sample_var
+                        all_log_vars += sample_log_var
+
+                        dec_h, dec_state = decode(z, dec_state, reuse)
+                        with tf.variable_scope('write', reuse=reuse):
+                            write_param = O.fc('fc_param', dec_h, 5)
+                            write_in = O.fc('fc', dec_h, (att_dim * att_dim * c)).reshape(-1, att_dim, att_dim, c)
+
+                        with tf.name_scope('write_step{}'.format(step)):
+                            cx, cy, delta, var, gamma = draw_opr.split_att_params(h, w, att_dim, write_param)
+                            write_out = draw_opr.att_write(h, w, write_in, cx, cy, delta, var)
+
+                        canvas += write_out
+
+                    canvas = O.sigmoid(canvas)
+
+                if env.phase is env.Phase.TRAIN:
+                    with tf.variable_scope('loss'):
+                        content_loss = O.raw_cross_entropy_prob('raw_content', img.flatten2(), canvas.flatten2())
+                        content_loss = content_loss.sum(axis=1).mean(name='content')
+                        # distrib_loss = 0.5 * (O.sqr(mu) + O.sqr(std) - 2. * O.log(std + 1e-8) - 1.0).sum(axis=1)
+                        distrib_loss = -0.5 * (float(nr_glimpse) + all_log_vars - all_sqr_mus - all_vars).sum(axis=1)
+                        distrib_loss = distrib_loss.mean(name='distrib') / float(nr_glimpse)
+
+                        summary.scalar('content_loss', content_loss)
+                        summary.scalar('distrib_loss', distrib_loss)
+
+                        loss = content_loss + distrib_loss
+                    dpc.add_output(loss, name='loss', reduce_method='sum')
+
+                dpc.add_output(canvas, name='output')
+
+            dpc.set_input_maker(inputs).set_forward_func(forward)
+
+        net.add_all_dpc_outputs(dpc, loss_name='loss')
+
+        if env.phase is env.Phase.TRAIN:
+            summary.inference.scalar('loss', net.loss)
+
+
+def make_optimizer(env):
+    wrapper = optimizer.OptimizerWrapper()
+    wrapper.set_base_optimizer(optimizer.base.AdamOptimizer(get_env('trainer.learning_rate')))
+    wrapper.append_grad_modifier(optimizer.grad_modifier.LearningRateMultiplier([
+        ('*/b', 2.0),
+    ]))
+    # wrapper.append_grad_modifier(optimizer.grad_modifier.WeightDecay([
+    #     ('*/W', 0.0005)
+    # ]))
+    env.set_optimizer(wrapper)
+
+
+from data_provider_vae_mnist import *
+
+
+def main_train(trainer):
+    from tartist.plugins.trainer_enhancer import summary
+    summary.enable_summary_history(trainer)
+    summary.enable_echo_summary_scalar(trainer)
+
+    from tartist.plugins.trainer_enhancer import progress
+    progress.enable_epoch_progress(trainer)
+
+    from tartist.plugins.trainer_enhancer import snapshot
+    snapshot.enable_snapshot_saver(trainer)
+
+    from tartist.plugins.trainer_enhancer import inference
+    inference.enable_inference_runner(trainer, make_dataflow_inference)
+
+    trainer.train()
---
+++ examples/generative-model/draw_opr.py
@@ -0,0 +1,141 @@
+# -*- coding:utf8 -*-
+# File   : draw_opr.py
+# Author : Jiayuan Mao
+# Email  : maojiayuan@gmail.com
+# Date   : 3/22/17
+# 
+# This file is part of TensorArtist
+
+import tensorflow as tf
+from tartist.nn import opr as O
+
+
+def image_diff(origin, canvas_logits):
+    """
+    get the difference between original image and the canvas,
+    note that the canvas is given without sigmoid activation (we will do it inside)
+    :param origin: original image: batch_size, h, w, c
+    :param canvas_logits: canvas logits: batch_size, h, w, c
+    :return: the difference: origin - sigmoid(logits)
+    """
+    sigmoid_canvas = O.sigmoid(canvas_logits)
+    return origin - sigmoid_canvas
+
+
+def filterbank(img_h, img_w, att_dim, center_x, center_y, delta, var):
+    """
+    get the filterbank matrix
+    :param img_h: image height
+    :param img_w: image width
+    :param att_dim: attention dim, the attention window is att_dim x att_dim
+    :param center_x: attention center (x-axis): batch_size x 1
+    :param center_y: attention center (y-axis): batch_size x 1
+    :param delta: stride: batch_size x 1
+    :param var: variance (sigma^2): batch_size x 1
+    :return: filter_x, filter_y
+    """
+
+    with tf.name_scope('filterbank'):
+        rng = O.range(1, att_dim+1, dtype='float32') - att_dim / 2 + 0.5
+        mu_x = center_x + rng * delta
+        mu_y = center_y + rng * delta
+        all_x = O.range(1, img_w+1, dtype='float32')
+        all_y = O.range(1, img_h+1, dtype='float32')
+        a = all_x - mu_x.add_axis(-1)
+        b = all_y - mu_y.add_axis(-1)
+
+        fx = O.exp(-O.sqr(a) / var.add_axis(-1) / 2.)
+        fy = O.exp(-O.sqr(b) / var.add_axis(-1) / 2.)
+
+        fx /= fx.sum(axis=2, keepdims=True) + 1e-8
+        fy /= fy.sum(axis=2, keepdims=True) + 1e-8
+
+        fx = O.as_varnode(fx)
+
+        return fx, fy
+
+
+def split_att_params(img_h, img_w, att_dim, value):
+    """
+    split attention params
+    :param img_h: image height
+    :param img_w: image width
+    :param att_dim: attention dim, the attention window is att_dim x att_dim
+    :param value: attention params produced by hidden layer: batch_size, 5
+    :return: center_x, center_y, delta, variance, gamma
+    """
+
+    with tf.name_scope('split_att_params'):
+        center_x, center_y, log_delta, log_var, log_gamma = O.split(value, 5, axis=1)
+
+        delta, var, gamma = map(lambda x: O.exp(x).reshape(-1, 1), [log_delta, log_var, log_gamma])
+        center_x = ((img_w + 1.) * (center_x + 1.) / 2.).reshape(-1, 1)
+        center_y = ((img_h + 1.) * (center_y + 1.) / 2.).reshape(-1, 1)
+        delta *= float((max(img_h, img_w) - 1) / (att_dim - 1))
+        return center_x, center_y, delta, var, gamma
+
+
+def apply_filterbank(inpvar, fx, fy, dir):
+    """
+    apply the filter bank, if dir is i2w, then inp var is treated as image, otherwise it is treated as window
+    :param inpvar: image: batch_size x h x w x channel
+    :param fx: filter_x: batch_size x att_dim x w
+    :param fy: filter_y: batch_size x att_dim x h
+    :param dir: either "i2w" or "w2i" (image to window / window to image)
+    :return: filtered image: batch_size, att_dim, att_dim, channel
+    """
+
+    assert dir in ('i2w', 'w2i')
+
+    def add_channel(x, c):
+        return O.tile(x, [c, 1, 1])
+
+    with tf.name_scope('apply_filterbank'):
+        h, w, c = inpvar.static_shape[1:4]
+        img_h, img_w = fy.static_shape[2], fx.static_shape[2]
+        att_dim = fx.static_shape[1]
+        assert h is not None and w is not None and c is not None, inpvar.static_shape
+        inpvar = inpvar.dimshuffle((0, 3, 1, 2)).reshape(-1, h, w)
+        fx, fy = add_channel(fx, c), add_channel(fy, c)
+
+        if dir == 'i2w':
+            window = O.batch_matmul(O.batch_matmul(fy, inpvar), fx.dimshuffle(0, 2, 1))
+            return window.reshape(-1, c, att_dim, att_dim).dimshuffle(0, 2, 3, 1)
+        else:
+            image = O.batch_matmul(O.batch_matmul(fy.dimshuffle(0, 2, 1), inpvar), fx)
+            return image.reshape(-1, c, img_h, img_w).dimshuffle(0, 2, 3, 1)
+
+
+def att_read(att_dim, image, center_x, center_y, delta, var):
+    """
+    perform attention reading given center, delta and variance
+    :param att_dim: attention dim, the attention window is att_dim x att_dim
+    :param image: image: batch_size x h x w x c
+    :param center_x: attention center (x-axis): batch_size x 1
+    :param center_y: attention center (y-axis): batch_size x 1
+    :param delta: stride: batch_size x 1
+    :param var: variance (sigma^2): batch_size x 1
+    :return: attention window: batch_size x att_dim x att_dim x channel
+    """
+
+    with tf.name_scope('att_read'):
+        fx, fy = filterbank(image.static_shape[1], image.static_shape[2], att_dim, center_x, center_y, delta, var)
+        return apply_filterbank(image, fx, fy, dir='i2w')
+
+
+def att_write(img_h, img_w, window, center_x, center_y, delta, var):
+    """
+    perform attention writing given center, delta, and variance
+    :param img_h: image height
+    :param img_w: image width
+    :param window: window to write: batch_size x att_dim x att_dim x c
+    :param center_x: attention center (x-axis): batch_size x 1
+    :param center_y: attention center (y-axis): batch_size x 1
+    :param delta: stride: batch_size x 1
+    :param var: variance (sigma^2): batch_size x 1
+    :return: a delta canvas
+    """
+    with tf.name_scope('att_write'):
+        fx, fy = filterbank(img_h, img_w, window.static_shape[1], center_x, center_y, delta, var)
+        return apply_filterbank(window, fx, fy, dir='w2i')
+
