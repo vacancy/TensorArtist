38fcbd39b307b80449ea66fb4cbe1f078eb6e8f3
Jiayuan MAO <maojiayuan@gmail.com>
2017-03-25 18:20:49 -0700

    [master] update docs server

--- scripts/docs-server.py
+++ scripts/docs-server.py
@@ -13,17 +13,24 @@ import sys
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--port', dest='port', default=8080, type=int, help='Serving port')
+parser.add_argument('-p', '--port', dest='port', default=8080, type=int, help='Serving port.')
+parser.add_argument('-m', '--make', dest='make', default=True, action='store_true', help='Run the makefile.')
 port = parser.parse_args().port
 
 if __name__ == '__main__':
     dirname = os.path.dirname(sys.argv[0])
+
+    docpath = os.path.realpath(os.path.join(dirname, '../', 'docs'))
+    os.chdir(docpath)
+    print('Chaning directory to {}'.format(docpath))
+    os.system('make html')
+
     docpath = os.path.realpath(os.path.join(dirname, '../', 'docs', '_build', 'html'))
     os.chdir(docpath)
+    print('Chaning directory to {}'.format(docpath))
 
     Handler = http.server.SimpleHTTPRequestHandler
 
     with socketserver.TCPServer(("", port), Handler) as httpd:
-        print('Chaning directory to {}'.format(docpath))
         print('Serving at port {}.'.format(port))
         httpd.serve_forever()
