6a5fb34be4333dfb79abaa0a88026b6b53d57508
Honghua Dong <dhh19951@gmail.com>
2017-04-11 22:54:09 -0700

    [dev-dhh] add nonlin/bn_leaky_relu

--- tartist/nn/opr/nonlin.py
+++ tartist/nn/opr/nonlin.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 from .helper import as_varnode, get_4dshape, get_2dshape, wrap_varnode_func, wrap_simple_named_op
 from .helper import lazy_O as O
 
-__all__ = ['p_relu', 'leaky_relu', 'bn_relu', 'bn_nonlin']
+__all__ = ['p_relu', 'leaky_relu', 'bn_relu', 'bn_leaky_relu', 'bn_nonlin']
 
 
 @wrap_simple_named_op
@@ -35,6 +35,14 @@ def leaky_relu(x, alpha, name='leaky_relu'):
 def bn_relu(inpvar, name='bn_relu'):
     _ = O.batch_norm('bn', inpvar)
     _ = O.relu(_, name='relu')
+    return O.identity(_, name='out')
+
+
+@wrap_simple_named_op
+@wrap_varnode_func
+def bn_leaky_relu(inpvar, alpha, name='bn_leaky_relu'):
+    _ = O.batch_norm('bn', inpvar)
+    _ = O.leaky_relu(_, alpha, name='leaky_relu')
     return O.identity(_, name='out')
 
 
