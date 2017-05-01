266ce8299cc87c7456c46b455eb7ce053a60931e
Jiayuan Mao <maojiayuan@gmail.com>
2017-04-30 18:06:58 -0700

    [dev-mjy] output naming

--- tartist/nn/opr/arith.py
+++ tartist/nn/opr/arith.py
@@ -15,26 +15,26 @@ __all__ = ['rms', 'std', 'atanh', 'logit']
 @wrap_simple_named_op
 @wrap_varnode_func
 def rms(inpvar, name='rms'):
-    return O.sqrt((as_varnode(inpvar) ** 2.).mean(), name=name)
+    return O.sqrt((as_varnode(inpvar) ** 2.).mean(), name='out')
 
 
 @wrap_simple_named_op
 @wrap_varnode_func
 def std(inpvar, name='std'):
     inpvar = as_varnode(inpvar)
-    return O.sqrt(((inpvar - inpvar.mean()) ** 2.).mean(), name=name)
+    return O.sqrt(((inpvar - inpvar.mean()) ** 2.).mean(), name='out')
 
 
 @wrap_simple_named_op
 @wrap_varnode_func
 def atanh(inpvar, name='atanh', eps=1e-6):
     inpvar = as_varnode(inpvar)
-    return 0.5 * O.log((1. + inpvar) / (1. - inpvar + eps) + eps)
+    return O.identity(0.5 * O.log((1. + inpvar) / (1. - inpvar + eps) + eps), name='out')
 
 
 @wrap_simple_named_op
 @wrap_varnode_func
 def logit(inpvar, name='logit', eps=1e-6):
     inpvar = as_varnode(inpvar)
-    return 0.5 * O.log(inpvar / (1. - inpvar + eps) + eps)
+    return O.identity(0.5 * O.log(inpvar / (1. - inpvar + eps) + eps), name='out')
 
