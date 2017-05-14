bd85e2e6e4cd9663bb060d0d8242c0f358b5974e
Jiayuan Mao <maojiayuan@gmail.com>
2017-05-13 17:08:14 -0700

    [dev-mjy] including more packages in cao

--- tartist/cao/shortcut.py
+++ tartist/cao/shortcut.py
@@ -13,6 +13,8 @@ from ..core import io, get_logger
 from ..core import get_env, set_env
 from ..core import register_event, trigger_event
 from ..core.utils.cli import load_desc, parse_devices
+from ..app import gan, rl
+from ..data import flow, rflow
 from ..nn import opr as O
 from ..nn.opr import callback_injector as cbi
 from ..nn.graph import Env, Network, DataParallelController, get_default_net, get_default_env
@@ -28,8 +30,10 @@ __all__ = [
     'image', 'random', 'io', 'logger',
     'get_env', 'set_env',
     'register_event', 'trigger_event',
+    'gan', 'rl', 'flow', 'rflow',
     'load_desc', 'parse_devices', 'load_weights_file',
     'O', 'cbi', 'Env', 'Network', 'DataParallelController', 'get_default_env', 'get_default_net',
     'Function', 'VarNode', 'OprNode', 'as_tftensor', 'as_varnode',
     'np', 'tf'
 ]
+
