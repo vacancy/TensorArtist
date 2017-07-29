78ceb7e098637d1e0fd52fd93a9d3e892dc35627
Jiayuan Mao <maojiayuan@gmail.com>
2017-07-29 02:43:31 -0700

    [dev-mjy] add status output at tart-name-server

--- scripts/name-server.py
+++ scripts/name-server.py
@@ -6,9 +6,12 @@
 # 
 # This file is part of TensorArtist.
 
+from tartist.core import get_logger
 from tartist.data.rflow.name_server import NameServer
 from tartist.data.rflow import configs
 import argparse
+
+logger = get_logger(__file__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--host', dest='host', default=configs.NS_CTL_HOST)
@@ -18,4 +21,5 @@ args = parser.parse_args()
 
 
 if __name__ == '__main__':
+    logger.critical('Starting name server at {}://{}:{}.'.format(args.protocal, args.host, args.port))
     NameServer(host=args.host, port=args.port, protocal=args.protocal).mainloop()
