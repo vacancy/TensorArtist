f90b3df1d239e00ff8ea3054ca05ed356af63849
dhh1995 <dhh1995@163.com>
2017-08-10 23:15:15 -0700

    [dev-dhh] GymNintendoWrapper

--- tartist/app/rl/gym.py
+++ tartist/app/rl/gym.py
@@ -32,7 +32,7 @@ _ENV_LOCK = threading.Lock()
 def get_env_lock():
     return _ENV_LOCK
 
-__all__ = ['GymRLEnviron', 'GymHistoryProxyRLEnviron', 'GymPreventStuckProxyRLEnviron']
+__all__ = ['GymRLEnviron', 'GymHistoryProxyRLEnviron', 'GymPreventStuckProxyRLEnviron', 'GymNintendoWrapper']
 
 
 class GymRLEnviron(SimpleRLEnvironBase):
@@ -133,3 +133,35 @@ class GymPreventStuckProxyRLEnviron(ProxyRLEnvironBase):
     def _restart(self, *args, **kwargs):
         super()._restart(*args, **kwargs)
         self._action_list.clear()
+
+# https://github.com/ppaquette/gym-super-mario/blob/master/ppaquette_gym_super_mario/wrappers/action_space.py
+from gym_adapter import DiscreteToMultiDiscrete
+class GymNintendoWrapper(gym.Wrapper):
+    """
+        Wrapper to convert MultiDiscrete action space to Discrete
+
+        Only supports one config, which maps to the most logical discrete space possible
+    """
+    def __init__(self, env):
+        super(ToDiscreteWrapper, self).__init__(env)
+        # Nintendo Game Controller
+        mapping = {
+            0: [0, 0, 0, 0, 0, 0],  # NOOP
+            1: [1, 0, 0, 0, 0, 0],  # Up
+            2: [0, 0, 1, 0, 0, 0],  # Down
+            3: [0, 1, 0, 0, 0, 0],  # Left
+            4: [0, 1, 0, 0, 1, 0],  # Left + A
+            5: [0, 1, 0, 0, 0, 1],  # Left + B
+            6: [0, 1, 0, 0, 1, 1],  # Left + A + B
+            7: [0, 0, 0, 1, 0, 0],  # Right
+            8: [0, 0, 0, 1, 1, 0],  # Right + A
+            9: [0, 0, 0, 1, 0, 1],  # Right + B
+            10: [0, 0, 0, 1, 1, 1],  # Right + A + B
+            11: [0, 0, 0, 0, 1, 0],  # A
+            12: [0, 0, 0, 0, 0, 1],  # B
+            13: [0, 0, 0, 0, 1, 1],  # A + B
+        }
+        self.action_space = DiscreteToMultiDiscrete(self.action_space, mapping)
+
+    def _step(self, action):
+        return self.env._step(self.action_space(action))
