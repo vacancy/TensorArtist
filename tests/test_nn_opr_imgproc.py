1e26aaf7246aec7e36e1f82b84d4f636c7d3b311
Jiayuan MAO <maojiayuan@gmail.com>
2017-10-10 17:07:13 +0800

    [dev-mjy] remove space at eol

--- tests/test_nn_opr_imgproc.py
+++ tests/test_nn_opr_imgproc.py
@@ -3,7 +3,7 @@
 # Author : Jiayuan Mao
 # Email  : maojiayuan@gmail.com
 # Date   : 10/10/17
-# 
+#
 # This file is part of TensorArtist.
 
 from tartist.nn import Env, opr as O
@@ -29,9 +29,9 @@ class TestNNOprImgproc(unittest.TestCase):
     @wraps_new_env
     def testCropCenter(self):
         a = O.placeholder('a', shape=(16, 17, 17, 3))
-        b = O.crop_center(a, [15, 15]) 
+        b = O.crop_center(a, [15, 15])
         self.assertTupleEqual(b.static_shape, (16, 15, 15, 3))
-        
+
         avar = np.random.normal(size=(16, 17, 17, 3))
         bvar = avar[:, 1:-1, 1:-1, :]
         self.assertTensorClose(b.eval(a=avar), bvar)
@@ -39,9 +39,9 @@ class TestNNOprImgproc(unittest.TestCase):
     @wraps_new_env
     def testCropLU(self):
         a = O.placeholder('a', shape=(16, 17, 17, 3))
-        b = O.crop_lu(a, [15, 15]) 
+        b = O.crop_lu(a, [15, 15])
         self.assertTupleEqual(b.static_shape, (16, 15, 15, 3))
-        
+
         avar = np.random.normal(size=(16, 17, 17, 3))
         bvar = avar[:, :-2, :-2, :]
         self.assertTensorClose(b.eval(a=avar), bvar)
@@ -49,9 +49,9 @@ class TestNNOprImgproc(unittest.TestCase):
     @wraps_new_env
     def testPaddingCenter(self):
         a = O.placeholder('a', shape=(16, 15, 15, 3))
-        b = O.pad_center(a, [17, 17]) 
+        b = O.pad_center(a, [17, 17])
         self.assertTupleEqual(b.static_shape, (16, 17, 17, 3))
-        
+
         avar = np.random.normal(size=(16, 15, 15, 3))
         bvar = np.pad(avar, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='constant')
         self.assertTensorClose(b.eval(a=avar), bvar)
@@ -59,9 +59,9 @@ class TestNNOprImgproc(unittest.TestCase):
     @wraps_new_env
     def testPaddingRB(self):
         a = O.placeholder('a', shape=(16, 15, 15, 3))
-        b = O.pad_rb(a, [16, 16]) 
+        b = O.pad_rb(a, [16, 16])
         self.assertTupleEqual(b.static_shape, (16, 16, 16, 3))
-        
+
         avar = np.random.normal(size=(16, 15, 15, 3))
         bvar = np.pad(avar, [[0, 0], [0, 1], [0, 1], [0, 0]], mode='constant')
         self.assertTensorClose(b.eval(a=avar), bvar)
@@ -69,13 +69,13 @@ class TestNNOprImgproc(unittest.TestCase):
     @wraps_new_env
     def testPaddingRBMultiple(self):
         a = O.placeholder('a', shape=(16, 15, 15, 3))
-        b = O.pad_rb_multiple_of(a, 8) 
+        b = O.pad_rb_multiple_of(a, 8)
         self.assertTupleEqual(b.static_shape, (16, 16, 16, 3))
-        
+
         avar = np.random.normal(size=(16, 15, 15, 3))
         bvar = np.pad(avar, [[0, 0], [0, 1], [0, 1], [0, 0]], mode='constant')
         self.assertTensorClose(b.eval(a=avar), bvar)
-       
+
 
 if __name__ == '__main__':
     unittest.main()
