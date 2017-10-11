a1f323e7f78428fbe998f7857a3cdc9435dd365e
Jiayuan Mao <maojiayuan@gmail.com>
2017-10-11 11:46:27 +0800

    [dev-mjy] add with_rng

--- tartist/random/rng.py
+++ tartist/random/rng.py
@@ -5,35 +5,47 @@
 # Date   : 2/23/17
 #
 # This file is part of TensorArtist.
-# This file is part of NeuArtist2
+# This file is part of NeuArtist2.
 
 import os
+import threading
+import contextlib
 
 import numpy as np
 import numpy.random as npr
 
-__all__ = ['rng', 'reset_rng', 'gen_seed', 'gen_rng', 'shuffle_multiarray']
+__all__ = ['get_rng', 'with_rng', 'reset_rng', 'gen_seed', 'gen_rng', 'shuffle_multiarray']
 
-rng = None
+_rng = None
+_rng_local = threading.local()
 
 
-def __initialize_rng():
-    seed = os.getenv('TART_RANDOM_SEED')
-    reset_rng(seed)
+def get_rng():
+    if getattr(_rng_local, 'rng', None) is None:
+        return _rng
+    return _rng_local.rng
 
 
 def reset_rng(seed=None):
-    global rng
-    if rng is None:
-        rng = npr.RandomState(seed)
+    """Reset the program-level random generator, non-threading-safe function."""
+    global _rng
+    if _rng is None:
+        _rng = npr.RandomState(seed)
     else:
         rng2 = npr.RandomState(seed)
-        rng.set_state(rng2.get_state())
+        _rng.set_state(rng2.get_state())
+
+
+@contextlib.contextmanager
+def with_rng(rng):
+    old_rng = getattr(_rng_local, 'rng', None)
+    _rng_local.rng = rng
+    yield rng
+    _rng_local.rng = old_rng
 
 
 def gen_seed():
-    global rng
-    return rng.randint(4294967296)
+    return _rng.randint(4294967296)
 
 
 def gen_rng(seed=None):
@@ -42,7 +54,8 @@ def gen_rng(seed=None):
     return npr.RandomState(seed)
 
 
-def shuffle_multiarray(*arrs):
+def shuffle_multiarray(*arrs, rng=None):
+    rng = rng or _rng
     length = len(arrs[0])
     for a in arrs:
         assert len(a) == length, 'non-compatible length when shuffling multiple arrays'
@@ -50,5 +63,28 @@ def shuffle_multiarray(*arrs):
     inds = np.arange(length)    
     rng.shuffle(inds)
     return tuple(map(lambda x: x[inds], arrs))
+
+
+def list_choice(l, rng=None):
+    """Efficiently draw an element from an list, if the rng is given, use it instead of the system one."""
+    rng = rng or _rng
+    assert type(l) in (list, tuple)
+    return l[rng.choice(len(l))]
+
+
+def list_shuffle(l, rng=None):
+    rng = rng or _rng
+    if isndarray(l):
+        rng.shuffle(l)
+        return
+
+    assert type(l) is list
+    _random.shuffle(l, random=rng.random_sample)
+
+
+def __initialize_rng():
+    seed = os.getenv('TART_RANDOM_SEED')
+    reset_rng(seed)
+
 
 __initialize_rng()
